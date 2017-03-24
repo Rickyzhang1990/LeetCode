42817001e4ad5b20dd7ce0b822a64e5a3fd7c236
Rickyzhang1990 <fredzhang1990@163.com>
2017-03-24 14:56:12 +0800

    Update Largest_Palindrome_Product.py

--- questions/Largest_Palindrome_Product.py
+++ questions/Largest_Palindrome_Product.py
@@ -1,1 +1,21 @@
-
+class Solution(object):
+    def longestPalindrome(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        dic = {}
+        count = 0
+        for x in s:
+            if x in dic:
+                dic[x] += 1
+            else:
+                dic[x] =1 
+        for i in dic:
+            if dic[i] %2 != 0 :
+                count += 1
+        if len(dic) >1 and count != 0:
+            length = len(s) - count + 1
+        else:
+            length = len(s)
+        return length
