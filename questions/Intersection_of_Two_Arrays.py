d7ca506a6ba2c0aba418666d20df346a00d1a9f6
Rickyzhang1990 <fredzhang1990@163.com>
2017-03-24 14:57:49 +0800

    Update Intersection_of_Two_Arrays.py

--- questions/Intersection_of_Two_Arrays.py
+++ questions/Intersection_of_Two_Arrays.py
@@ -1,2 +1,11 @@
 #!/usr/bin/env python 
 #conding=utf-8
+class Solution(object):
+    def intersection(self, nums1, nums2):
+        """
+        :type nums1: List[int]
+        :type nums2: List[int]
+        :rtype: List[int]
+        """
+        intersect = list(set(nums1)&set(nums2))
+        return intersect 
