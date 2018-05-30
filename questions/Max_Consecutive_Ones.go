34883acebf9e30b3a60ead2d167e4da062665cca
Rickyzhang1990.github.io <fredzhang1990@163.com>
2018-05-30 15:31:57 +0800

    Create Max_Consecutive_Ones.go

---
+++ questions/Max_Consecutive_Ones.go
@@ -0,0 +1,26 @@
+func findMaxConsecutiveOnes(nums []int) int {
+    var lenght = 0
+    var len_list [] int 
+    for _,j := range(nums){
+        switch {
+            case j == 0 :
+            len_list = append(len_list ,[] int{lenght} ...)
+            lenght = 0
+            continue 
+            case j == 1 :
+            lenght++
+        }
+        len_list = append(len_list ,[] int {lenght} ...)
+    }
+    return max(len_list)
+}
+
+func max(list [] int ) int {
+    first := list[0]
+    for _ , n := range(list){
+        if first < n{
+            first = n 
+        }
+    }
+    return first 
+}
