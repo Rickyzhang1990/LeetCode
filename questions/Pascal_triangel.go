41a6cc9580415dd5be4041e785bb75af4b76ecdf
chaozhang <fredzhang1990@163.com>
2018-06-01 15:02:13 +0800

    Create Pascal_triangel.go

---
+++ questions/Pascal_triangel.go
@@ -0,0 +1,26 @@
+func generate(numRows int) [][]int {
+    list := make([][] int , numRows)
+   switch {
+       case numRows == 0:
+        list = [] [] int {}
+       case numRows == 1:
+        list[0] =  [] int {1}
+       case numRows > 1 :
+        for i := 0;i < numRows;i++{
+            if i == 0 || i== 1 {
+                list[0] ,list[1] = [] int {1} ,[] int {1,1}
+            }else {
+            tmp := make([] int ,i+1)  
+            for j := 0 ;j < i+1 ;j++{
+                if j == 0 || j == i{
+                    tmp[0] , tmp[i] = 1,1
+                }else{
+                    tmp[j] = list[i-1][j-1] + list[i-1][j]
+                }
+              }  
+           list[i] = tmp
+            }
+          }
+        }
+    return list 
+    }
