5be6ddc94cee9c6b608e273e3538e5b72226674b
chaozhang <fredzhang1990@163.com>
2018-06-06 21:10:40 +0800

    fix some code

--- questions/keyboard_row.py
+++ questions/keyboard_row.py
@@ -4,9 +4,9 @@ class Solution:
         :type words: List[str]
         :rtype: List[str]
         """
-        lineA = ["Q","q","W","w","E","e","R","r","T","t","Y","y","U","u","I","i","O","o","P","p"]
-        lineB = ["A","a","S","s","D","d","F","f","G","g","H","h","J","j","K","k","L","l"]
-        lineC = ["Z","z","X","x","C","c","V","v","B","b","N","n","M","m"]
+        lineA = "QWERTYUIOP"
+        lineB = "ASDFGHJKL"
+        lineC = "ZXCVBNM"
         result = []
         for word in words:
             letters = list(word)
@@ -14,13 +14,13 @@ class Solution:
             resultB = []
             resultC = []
             for letter in letters:
-                if letter in lineA:
+                if letter.upper() in lineA:
                     resultA.append(1)
                 else:   resultA.append(0)
-                if letter in lineB: 
+                if letter.upper() in lineB: 
                     resultB.append(1)
                 else:   resultB.append(0)
-                if letter in lineC: 
+                if letter.upper() in lineC: 
                     resultC.append(1)
                 else:   resultC.append(0)
             if any([all(resultA),all(resultB),all(resultC)]):
